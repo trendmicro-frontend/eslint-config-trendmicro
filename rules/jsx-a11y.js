2f4347ddec09e195d75974a6aab0bd6f4f9d3ce3
Cheton Wu <cheton@gmail.com>
2017-08-29 15:29:47 +0800

    'jsx-a11y/href-no-hash' will be removed in jsx-a11y v6

--- rules/jsx-a11y.js
+++ rules/jsx-a11y.js
@@ -4,6 +4,7 @@ module.exports = {
         'react'
     ],
     rules: {
+        'jsx-a11y/href-no-hash': 1,
         'jsx-a11y/label-has-for': 0,
         'jsx-a11y/no-static-element-interactions': 1
     }
