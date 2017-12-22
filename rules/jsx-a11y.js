087a75bb19a7cb64a7a3443aeb9db51576b59d09
Cheton Wu <cheton@gmail.com>
2017-12-22 14:33:53 +0800

    Update rules

--- rules/jsx-a11y.js
+++ rules/jsx-a11y.js
@@ -4,7 +4,7 @@ module.exports = {
         'react'
     ],
     rules: {
-        'jsx-a11y/href-no-hash': 1,
+        'jsx-a11y/anchor-is-valid': 0,
         'jsx-a11y/label-has-for': 0,
         'jsx-a11y/no-static-element-interactions': 1
     }
