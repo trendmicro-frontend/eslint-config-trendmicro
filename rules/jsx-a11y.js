e552588ae73706045a9f755202bf460013574a9c
Cheton Wu <cheton@gmail.com>
2017-12-27 18:42:05 +0800

    List all accessibility rules in favor of eslint-plugin-jsx-a11y and eslint-config-airbnb

--- rules/jsx-a11y.js
+++ rules/jsx-a11y.js
@@ -1,11 +1,152 @@
+// https://github.com/evcohen/eslint-plugin-jsx-a11y/tree/master/docs/rules
 module.exports = {
     plugins: [
         'jsx-a11y',
         'react'
     ],
     rules: {
-        'jsx-a11y/anchor-is-valid': 0,
-        'jsx-a11y/label-has-for': 0,
-        'jsx-a11y/no-static-element-interactions': 1
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/accessible-emoji.md
+        'jsx-a11y/accessible-emoji': 'error',
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/alt-text.md
+        'jsx-a11y/alt-text': ['error', {
+            elements: ['img', 'object', 'area', 'input[type="image"]'],
+            img: [],
+            object: [],
+            area: [],
+            'input[type="image"]': []
+        }],
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-has-content.md
+        'jsx-a11y/anchor-has-content': ['error', { components: [] }],
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md
+        'jsx-a11y/anchor-is-valid': ['error', {
+            components: ['Link'],
+            specialLink: ['to'],
+            aspects: ['noHref', 'invalidHref', 'preferButton']
+        }],
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/aria-activedescendant-has-tabindex.md
+        'jsx-a11y/aria-activedescendant-has-tabindex': 'error',
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/aria-props.md
+        'jsx-a11y/aria-props': 'error',
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/aria-proptypes.md
+        'jsx-a11y/aria-proptypes': 'error',
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/aria-role.md
+        'jsx-a11y/aria-role': ['error', { ignoreNonDom: false }],
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/aria-unsupported-elements.md
+        'jsx-a11y/aria-unsupported-elements': 'error',
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/click-events-have-key-events.md
+        'jsx-a11y/click-events-have-key-events': 'warn',
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/heading-has-content.md
+        'jsx-a11y/heading-has-content': ['error', { components: [''] }],
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/html-has-lang.md
+        'jsx-a11y/html-has-lang': 'error',
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/iframe-has-title.md
+        'jsx-a11y/iframe-has-title': 'error',
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/img-redundant-alt.md
+        'jsx-a11y/img-redundant-alt': 'error',
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/interactive-supports-focus.md
+        'jsx-a11y/interactive-supports-focus': 'error',
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/label-has-for.md
+        'jsx-a11y/label-has-for': ['off', { components: ['label'] }],
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/lang.md
+        'jsx-a11y/lang': 'error',
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/media-has-caption.md
+        'jsx-a11y/media-has-caption': ['error', {
+            audio: [],
+            video: [],
+            track: []
+        }],
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/mouse-events-have-key-events.md
+        'jsx-a11y/mouse-events-have-key-events': 'warn',
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/no-access-key.md
+        'jsx-a11y/no-access-key': 'error',
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/no-autofocus.md
+        'jsx-a11y/no-autofocus': ['error', { ignoreNonDOM: true }],
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/no-distracting-elements.md
+        'jsx-a11y/no-distracting-elements': ['error', {
+            elements: ['marquee', 'blink']
+        }],
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/no-interactive-element-to-noninteractive-role.md
+        'jsx-a11y/no-interactive-element-to-noninteractive-role': ['error', {
+            tr: ['none', 'presentation']
+        }],
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/no-noninteractive-element-interactions.md
+        'jsx-a11y/no-noninteractive-element-interactions': ['error', {
+            handlers: [
+                'onClick',
+                'onMouseDown',
+                'onMouseUp',
+                'onKeyPress',
+                'onKeyDown',
+                'onKeyUp'
+            ]
+        }],
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/no-noninteractive-element-to-interactive-role.md
+        'jsx-a11y/no-noninteractive-element-to-interactive-role': ['error', {
+            ul: ['listbox', 'menu', 'menubar', 'radiogroup', 'tablist', 'tree', 'treegrid'],
+            ol: ['listbox', 'menu', 'menubar', 'radiogroup', 'tablist', 'tree', 'treegrid'],
+            li: ['menuitem', 'option', 'row', 'tab', 'treeitem'],
+            table: ['grid'],
+            td: ['gridcell']
+        }],
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/no-noninteractive-tabindex.md
+        'jsx-a11y/no-noninteractive-tabindex': ['error', {
+            tags: [],
+            roles: ['tabpanel']
+        }],
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/no-onchange.md
+        'jsx-a11y/no-onchange': 'off',
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/no-redundant-roles.md
+        'jsx-a11y/no-redundant-roles': 'error',
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/no-static-element-interactions.md
+        'jsx-a11y/no-static-element-interactions': ['error', {
+            handlers: [
+                'onClick',
+                'onMouseDown',
+                'onMouseUp',
+                'onKeyPress',
+                'onKeyDown',
+                'onKeyUp'
+            ]
+        }],
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/role-has-required-aria-props.md
+        'jsx-a11y/role-has-required-aria-props': 'error',
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/role-supports-aria-props.md
+        'jsx-a11y/role-supports-aria-props': 'error',
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/scope.md
+        'jsx-a11y/scope': 'error',
+
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/tabindex-no-positive.md
+        'jsx-a11y/tabindex-no-positive': 'error'
     }
 };
