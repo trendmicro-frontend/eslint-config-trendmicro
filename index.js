315b4db457e161a785ee22e71aa15e0877030158
Cheton Wu <chetonu@gmail.com>
2021-10-28 11:58:37 +0800

    feat: set ecmaVersion to 2020

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 module.exports = {
-    extends: [
-        'eslint-config-airbnb',
-        './rules/base',
-        './rules/import',
-        './rules/jsx-a11y',
-        './rules/react'
-    ].map(require.resolve),
-    parserOptions: {
-        ecmaVersion: 2017,
-        sourceType: 'module'
-    },
-    rules: {
-    }
+  extends: [
+    'eslint-config-airbnb',
+    './rules/base',
+    './rules/import',
+    './rules/jsx-a11y',
+    './rules/react',
+  ].map(require.resolve),
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+  }
 };
