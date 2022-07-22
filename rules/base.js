83acda8ddcf77fdff93759127bba85d570e9dfbf
cheton <cheton@gmail.com>
2022-07-22 11:33:34 +0800

    feat: omit `lines-between-class-members` rule

--- rules/base.js
+++ rules/base.js
@@ -40,7 +40,7 @@ module.exports = {
       'beforeColon': false,
       'afterColon': true
     }],
-    'lines-between-class-members': 1,
+    'lines-between-class-members': 0,
     'max-classes-per-file': [2, 1],
     'max-len': [1, 160, 4, {
       'ignoreComments': true,
