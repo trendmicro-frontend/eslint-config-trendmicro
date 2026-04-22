2dbf65d3470f6261390cf32b0a76a9bb6eeabb57
Cheton Wu <447801+cheton@users.noreply.github.com>
2026-04-22 15:54:11 +0800

    chore(rules): tune severities, add import rules, normalize style (#22)

--- rules/import.js
+++ rules/import.js
@@ -1,12 +1,14 @@
 export default {
   rules: {
     'import/exports-last': 0,
-    'import/named': 0,
-    'import/newline-after-import': 2,
+    'import/first': 2,
+    'import/named': 1,
+    'import/newline-after-import': 0,
+    'import/no-duplicates': 2,
     'import/no-extraneous-dependencies': 0,
     'import/no-named-as-default': 2,
     'import/no-relative-parent-imports': 0,
     'import/no-webpack-loader-syntax': 0,
-    'import/prefer-default-export': 0
+    'import/prefer-default-export': 0,
   }
 };
